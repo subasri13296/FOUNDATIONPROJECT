561bf19a0e485a63773a14374790efee80d7eee7
subasri13296 <33505674+subasri13296@users.noreply.github.com>
2017-11-14 12:38:09 +0530

    Add files via upload

---
+++ foundation/Solution.java
@@ -0,0 +1,67 @@
+package foundation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import foundation.emp.Employee;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session session = sf.openSession();
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		session.beginTransaction();
+
+		//System.out.print("enter ID:");
+		//int id = Integer.valueOf(bf.readLine());
+
+		System.out.print("enter Nmae:");
+		String name = bf.readLine();
+
+		System.out.print("enter email:");
+		String email = bf.readLine();
+
+		System.out.print("enter phno:");
+		long phone = Long.valueOf(bf.readLine());
+
+		System.out.print("enter Basic salary:");
+		double basicSalary = Double.valueOf(bf.readLine());
+
+		System.out.print("enter HRA:");
+		float houseRentAllowance = Float.valueOf(bf.readLine());
+
+		System.out.print("enter TA:");
+		float travelAllowance = Float.valueOf(bf.readLine());
+
+		System.out.print("enter DA:");
+		float dearnessAllowance = Float.valueOf(bf.readLine());
+
+		System.out.print("enter PF:");
+		Float providentFund = Float.valueOf(bf.readLine());
+
+		
+		Employee employee = new Employee(name, email, phone,basicSalary,houseRentAllowance,travelAllowance,dearnessAllowance,providentFund);
+		System.out.println("Id:" + employee.getId());
+		System.out.println("name:" + employee.getName());
+		System.out.println("Email:" + employee.getEmail());
+		System.out.println("Ph:" + employee.getPhone());
+		System.out.println("Basic Salary:" + employee.getBasicSalary());
+		System.out.println("HRA:" + employee.getHouseRentAllowance());
+		System.out.println("TA:" + employee.getTravelAllowance());
+		System.out.println("DA:" + employee.getDearnessAllowance());
+		System.out.println("PF:" + employee.getProvidentFund());
+		System.out.println("PF:" + employee.calculate(basicSalary, houseRentAllowance, travelAllowance, dearnessAllowance, providentFund););
+		session.save(employee);
+		session.getTransaction().commit();
+
+		session.close();
+
+	}
+
+}
---
+++ foundation/emp/Employee.java
@@ -0,0 +1,137 @@
+package foundation.emp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "Emp_Details")
+public class Employee {
+
+	public Employee(String name, String email, long phone, double basicSalary, float houseRentAllowance,
+			float travelAllowance, float dearnessAllowance, float providentFund) {
+		super();
+
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.basicSalary = basicSalary;
+		this.houseRentAllowance = houseRentAllowance;
+		this.travelAllowance = travelAllowance;
+		this.dearnessAllowance = dearnessAllowance;
+		this.providentFund = providentFund;
+		//this.netSalary = netSalary;
+	}
+
+	@Id
+	@GeneratedValue
+	@Column(name = "Emp_Id")
+	private int id;
+	@Column(name = "Emp_Name", length = 150, nullable = false)
+	private String name;
+	@Column(name = "Email_Id", nullable = false, unique = true)
+	private String email;
+	@Column(name = "Ph_No", unique = true, nullable = false)
+	private long phone;
+	// private boolean gender;
+	@Column(name = "Basic_Salary", nullable = false)
+	private double basicSalary;
+	@Column(name = "HRA", nullable = false)
+	private float houseRentAllowance;
+	@Column(name = "TA", nullable = false)
+	private float travelAllowance;
+	@Column(name = "DA", nullable = false)
+	private float dearnessAllowance;
+	@Column(name = "PF", nullable = false)
+	private float providentFund;
+	@Transient
+	private double netSalary;
+
+	public Employee() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+
+	
+	public double getBasicSalary() {
+		return basicSalary;
+	}
+
+	public void setBasicSalary(double basicSalary) {
+		this.basicSalary = basicSalary;
+	}
+
+	public float getHouseRentAllowance() {
+		return houseRentAllowance;
+	}
+
+	public void setHouseRentAllowance(float houseRentAllowance) {
+		this.houseRentAllowance = houseRentAllowance;
+	}
+
+	public float getTravelAllowance() {
+		return travelAllowance;
+	}
+
+	public void setTravelAllowance(float travelAllowance) {
+		this.travelAllowance = travelAllowance;
+	}
+
+	public float getDearnessAllowance() {
+		return dearnessAllowance;
+	}
+
+	public void setDearnessAllowance(float dearnessAllowance) {
+		this.dearnessAllowance = dearnessAllowance;
+	}
+
+	public float getProvidentFund() {
+		return providentFund;
+	}
+
+	public void setProvidentFund(float providentFund) {
+		this.providentFund = providentFund;
+	}
+
+	public void calculate(double basicSalary, float houseRentAllowance, float travelAllowance, float dearnessAllowance,
+			float providentFund) {
+		netSalary = this.basicSalary+(
+				((this.houseRentAllowance + this.travelAllowance + this.dearnessAllowance - this.providentFund) / 100)
+						* this.basicSalary);
+	}
+
+};
