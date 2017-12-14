d6237420cbb17f2e906919c3250e3d0a9616f10e
subasri13296 <33505674+subasri13296@users.noreply.github.com>
2017-12-14 09:37:31 +0530

    Create UserProfile.java

---
+++ OneToOneAssociation/UserProfile.java
@@ -0,0 +1,39 @@
+package org.hash;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_profile")
+public class UserProfile {
+	@OneToOne(mappedBy="profile")
+	//@JoinColumn(name = "credential")
+	private UserCredential credential;
+
+	public UserCredential getCredential() {
+		return credential;
+	}
+
+	public void setCredential(UserCredential credential) {
+		this.credential = credential;
+	}
+
+	@Id
+	@GeneratedValue
+	@Column(name = "profile_id")
+	private int profId;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "last_name")
+	private String lastName;
+	@Column(name = "email_id")
+	private String email;
+	@Column(name = "phone_no")
+	private int phone;
+
+}
