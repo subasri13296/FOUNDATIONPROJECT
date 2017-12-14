9240363c8c777d6ebd4e36fd09897a3cbee6c672
subasri13296 <33505674+subasri13296@users.noreply.github.com>
2017-12-14 10:05:37 +0530

    Create UserCredential.java

---
+++ OneToMany Association/UserCredential.java
@@ -0,0 +1,61 @@
+package org.auth;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_credential")
+public class UserCredential {
+
+	@OneToMany
+	private Set<UserProfile> profiles = new HashSet<UserProfile>();
+
+	public Set<UserProfile> getProfiles() {
+		return profiles;
+	}
+
+	public void setProfiles(Set<UserProfile> profiles) {
+		this.profiles = profiles;
+	}
+
+	@Id
+	@Column(name = "username")
+	private String username;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "status")
+	private int status;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+}
