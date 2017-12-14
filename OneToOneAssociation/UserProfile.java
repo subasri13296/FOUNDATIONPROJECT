7c38dda67ef80bfb216d245e37cc6f12d6161579
subasri13296 <33505674+subasri13296@users.noreply.github.com>
2017-12-14 09:41:13 +0530

    Update UserProfile.java

--- OneToOneAssociation/UserProfile.java
+++ OneToOneAssociation/UserProfile.java
@@ -36,4 +36,44 @@ public class UserProfile {
 	@Column(name = "phone_no")
 	private int phone;
 
+	public int getProfId() {
+		return profId;
+	}
+
+	public void setProfId(int profId) {
+		this.profId = profId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
 }
